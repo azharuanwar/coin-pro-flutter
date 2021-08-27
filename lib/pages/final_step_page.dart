06362f42588b61dfeef6ffeaddb07edffdc11e43
Azharu Anwar <azharu.anwar@gmail.com>
2021-08-28 01:08:32 +0700

    commit

--- lib/pages/final_step_page.dart
+++ lib/pages/final_step_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:shamo_apps/pages/theme.dart';
 
 class FinalStepPage extends StatelessWidget {
